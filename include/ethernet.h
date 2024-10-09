7c4b11bf31cddfe8fc1e1e3b6347c6d71a14126b
florentinojink <florentinojink@163.com>
2024-10-09 10:51:14 +0800

    以太网类型主机字节序转为网络字节序，添加inet.h头文件

--- include/ethernet.h
+++ include/ethernet.h
@@ -1,12 +1,17 @@
+#ifndef ETHERNET_H_
+#define ETHERNET_H_
+#include <linux/if_ether.h>
 
 struct eth_hdr
 {
     unsigned char dmac[6];
     unsigned char smac[6];
-    short ethertype;
+    unsigned short ethertype;
     unsigned char payload[];
 } __attribute__((packed));
 
 struct eth_hdr *init_eth_hdr(char *buf);
 
 void print_eth_hdr(struct eth_hdr *hdr);
+
+#endif //ETHERNET_H_
