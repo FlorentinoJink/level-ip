b34c67bc61b9210ca5f27bf0ea92558fb0e619a0
florentinojink <florentinojink@163.com>
2024-10-09 10:11:26 +0800

    添加公共头文件

---
+++ include/syshead.h
@@ -0,0 +1,16 @@
+#ifndef SYSHEAD_H_
+#define SYSHEAD_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+
+#endif // SYSHEAD_H_
